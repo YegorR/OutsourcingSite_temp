97ed497fd3ebded382f52304d18d12695b25e810
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-08 17:47:05 +0700

    Переименовал название проекта

--- OutsourcingSite_temp/src/main/java/com/antikryptonite/outsourcing/OutsourcingApplication.java
+++ OutsourcingSite_temp/src/main/java/com/antikryptonite/outsourcing/OutsourcingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OutsourcingSiteTempApplication {
+public class OutsourcingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OutsourcingSiteTempApplication.class, args);
+		SpringApplication.run(OutsourcingApplication.class, args);
 	}
 
 }

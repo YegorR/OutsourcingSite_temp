9aca92f42564945523a918c95482cd9592a566d2
IvanVF <fedinivanvladimirovich@mail.ru>
2020-11-08 13:48:19 +0700

    Start version

---
+++ OutsourcingSite_temp/OutsourcingSite_temp/src/test/java/com/antikryptonite/OutsourcingSite_temp/OutsourcingSiteTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antikryptonite.OutsourcingSite_temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingSiteTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

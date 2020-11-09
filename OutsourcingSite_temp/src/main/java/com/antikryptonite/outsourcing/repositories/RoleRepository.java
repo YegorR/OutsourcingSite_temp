fe323c2df4928c0f349b0e1f8c277f2fc247dbda
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-09 10:06:02 +0700

    Добавил @Repository

--- OutsourcingSite_temp/src/main/java/com/antikryptonite/outsourcing/repositories/RoleRepository.java
+++ OutsourcingSite_temp/src/main/java/com/antikryptonite/outsourcing/repositories/RoleRepository.java
@@ -2,10 +2,12 @@ package com.antikryptonite.outsourcing.repositories;
 
 import com.antikryptonite.outsourcing.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Репозиторий роли
  */
+@Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
 
     /**
--- OutsourcingSite_temp/src/main/java/com/antikryptonite/outsourcing/repositories/UserRepository.java
+++ OutsourcingSite_temp/src/main/java/com/antikryptonite/outsourcing/repositories/UserRepository.java
@@ -2,11 +2,14 @@ package com.antikryptonite.outsourcing.repositories;
 
 import com.antikryptonite.outsourcing.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
 /**
  * Репозиторий пользователя
  */
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     /**
